893687aea391d805444ad3f62fc43cb8f8c0fe51
bagussubagja <bagussubagja17@gmail.com>
2023-05-12 12:58:48 +0700

    dev-WWA-2 : Add Home Fragment UI

--- app/src/main/java/com/mantequilla/walletwizardapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/mantequilla/walletwizardapp/fragments/HomeFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mantequilla.walletwizardapp.R
+import com.mantequilla.walletwizardapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var binding : FragmentHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -15,7 +17,8 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+       binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
